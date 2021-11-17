a18c3bf76751fb3bba773ab0316ca0adddcd4790
Darwin Álvarez <darwin.DA64@gmail.com>
2021-11-17 14:09:34 -0500

    Cambiando a bienbevnidonksanklsanAK

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bienvenidos
+          Bienvenidos a todos
         </p>
         <button>Inicio</button>
       </header>
