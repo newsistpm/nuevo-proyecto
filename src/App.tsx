47fade2c904daedb5563e90a339bd989f2bfc125
Darwin Álvarez <darwin.DA64@gmail.com>
2021-11-17 14:16:29 -0500

    commietjklsahjlsa

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bienvenidos a todos
+          Bienvenidos a todos sbhabsh
         </p>
         <button>Inicio</button>
       </header>
